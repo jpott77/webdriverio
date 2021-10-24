ac073d7993063fb787b4674e74e61e52d18302df
Julia Pottinger <jpottinger@JPottinger-MacBook-Pro.local>
2021-10-24 15:18:59 -0500

    update to match video tutorial

--- test/waitForEnable.test.js
+++ test/waitForEnable.test.js
@@ -4,8 +4,10 @@ describe("Wait For Enabled", function () {
     it('should wait for the input field to be enabled', () => {
         browser.url(`${browser.options.baseUrl}/dynamic_controls`)
         internetPage.clickEnableButton()
-        internetPage.inputEnabledField.waitForEnabled(4000)
+        internetPage.inputEnabledField.waitForEnabled({ timeout: 4000})
         assert.equal(true, internetPage.inputEnabledField.isEnabled())
+        wdioExpect(internetPage.inputEnabledField).toBeEnabled()
+        wdioExpect(internetPage.inputEnabledField).not.toBeDisabled()
         //browser.debug() Uncomment to test how the debug functionality works
     })
     it('should wait for the input field to be disabled', () => {
